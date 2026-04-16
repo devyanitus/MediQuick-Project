84a3bc8e535121a93def3026bfc3103747daacfb
A00336127 <a00336127@student.tus.ie>
2026-04-17 00:55:59 +0100

    Jenkins Karate Integration Fixes

--- karate-tests/src/test/resources/karate-config.js
+++ karate-tests/src/test/resources/karate-config.js
@@ -1,17 +1,7 @@
 function fn() {
 
-    var env = karate.env || 'docker';
-    karate.log('Running Karate in:', env);
-
     var config = {};
-
-    if (env === 'docker') {
-        config.baseUrl = 'http://api-gateway:8091';
-    } else {
-        config.baseUrl = 'http://api-gateway:8091'; // force docker
-    }
-
-    karate.log('Base URL:', config.baseUrl);
+    config.baseUrl = 'http://localhost:8091';
 
     return config;
 }
